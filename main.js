20453b8475854547688028c19dd123f56431ddd7
Henrique <teteminglin@gmail.com>
2026-06-10 20:14:10 -0300

    BaseMain.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // Arquivo para código javascript
 
 const API_URL = "https://6a29e952f59cb8f65f1dc3a5.mockapi.io/teste/:endpoint";
+
+const btnCadastrar = document.getElementById("btn-cadastrar");
+const inputNome = document.getElementById("input-nome");
+const inputQuantidade = document.getElementById("input-quantidade");
+const listaMaterial = document.getElementById("lista-materiais");
+//SOCOROO MEU DEUS
