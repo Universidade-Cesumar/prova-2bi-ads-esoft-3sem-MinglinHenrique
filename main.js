71e34405d3c7c07505f7af997c82dd72de2eed85
Henrique <teteminglin@gmail.com>
2026-06-10 20:29:45 -0300

    Progressão Main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Arquivo para código javascript
 
-const API_URL = "https://6a29e952f59cb8f65f1dc3a5.mockapi.io/teste/:endpoint";
+const API_URL = "https://6a29e952f59cb8f65f1dc3a5.mockapi.io/teste";
 
 const btnCadastrar = document.getElementById("btn-cadastrar");
 const inputNome = document.getElementById("input-nome");
@@ -11,11 +11,33 @@ async function carregarMateriais() {
     const resposta = await fetch(API_URL);
     const materiais = await resposta.json();
 
-    listaMateriais.innerHTML = "";
+    listaMaterial.innerHTML = "";
 
     materiais.forEach(material => {
-        listaMateriais.innerHTML += `
+        listaMaterial.innerHTML += `
             <li>${material.nome} - ${material.quantidade}</li>
         `;
     });
 }
+
+carregarMateriais();
+
+btnCadastrar.addEventListener("click", async () => {
+    const novoMaterial = {
+        nome: inputNome.value,
+        quantidade: inputQuantidade.value
+    };
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(novoMaterial)
+    });
+
+    inputNome.value = "";
+    inputQuantidade.value = "";
+
+    carregarMateriais();
+});
