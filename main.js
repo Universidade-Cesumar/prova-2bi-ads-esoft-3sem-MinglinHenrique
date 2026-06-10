399c2e3416dc0b6e0e96e60bfbe6d405079aafe6
Henrique <teteminglin@gmail.com>
2026-06-10 20:23:34 -0300

    Base 4

--- main.js
+++ main.js
@@ -6,4 +6,16 @@ const btnCadastrar = document.getElementById("btn-cadastrar");
 const inputNome = document.getElementById("input-nome");
 const inputQuantidade = document.getElementById("input-quantidade");
 const listaMaterial = document.getElementById("lista-materiais");
-//SOCOROO MEU DEUS
+
+async function carregarMateriais() {
+    const resposta = await fetch(API_URL);
+    const materiais = await resposta.json();
+
+    listaMateriais.innerHTML = "";
+
+    materiais.forEach(material => {
+        listaMateriais.innerHTML += `
+            <li>${material.nome} - ${material.quantidade}</li>
+        `;
+    });
+}
